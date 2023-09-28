021a50ae2c8fb0a305f1340edd5f07a406efe3f0
Cedric Ahumuza <ahumuzacedric@gmail.com>
2023-09-28 15:22:06 +0300

    changes

---
+++ process.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $recipientEmail = $_POST["recipient_email"]; // Retrieve the recipient's email from the hidden input field
+
+    // Create email headers
+    $headers = "From: $email" . "\r\n";
+    $headers .= "Reply-To: $email" . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8";
+
+    // Build the email content
+    $mailBody = "Name: $name\n";
+    $mailBody .= "Email: $email\n";
+    $mailBody .= "Subject: $subject\n\n";
+    $mailBody .= "Message:\n$message";
+
+    // Send the email
+    if (mail($recipientEmail, $subject, $mailBody, $headers)) {
+        echo "Thank you for your message! We'll get back to you soon.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
